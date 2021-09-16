d06747830799543fcd4855039ccefea8083722d1
matheus <lucasag0408@outlook.com>
2021-09-16 17:45:04 -0400

    chenge: form html for Form component of unform/web lib

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,38 +1,48 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
-
+import { Form } from '@unform/web';
 import { Container, Content, Background } from './style';
 import logoImg from '../../assets/img/logo.svg';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 
-const Signin: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImg} alt="GoBarber" />
+const Signin: React.FC = () => {
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  function handleSubmit(data: object): void {
+    console.log('DATA:', data);
+  }
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
 
-      <form>
-        <h1>Faça seu Logon</h1>
-        <Input icon={FiMail} name="email" placeholder="E-mail" />
-        <Input
-          icon={FiLock}
-          name="password"
-          placeholder="Senha"
-          type="password"
-        />
-        <Button type="submit">Entrar</Button>
-        <a href="forgot">Esqueci minha senha</a>
-      </form>
+        <Form onSubmit={() => handleSubmit}>
+          <h1>Faça seu Logon</h1>
 
-      <a className="create-acount" href="login">
-        <FiLogIn />
-        Criar conta
-      </a>
-    </Content>
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
 
-    <Background />
-  </Container>
-);
+          <Button type="submit">Entrar</Button>
+
+          <a href="forgot">Esqueci minha senha</a>
+        </Form>
+
+        <a className="create-acount" href="login">
+          <FiLogIn />
+          Criar conta
+        </a>
+      </Content>
+
+      <Background />
+    </Container>
+  );
+};
 
 export default Signin;
