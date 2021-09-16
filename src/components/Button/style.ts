20d5a75f7dc2640bc85e95c9e6ec19a0a472138a
matheus <lucasag0408@outlook.com>
2021-09-16 17:35:37 -0400

    ajusting styles in Button Component

--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -3,15 +3,15 @@ import { shade } from 'polished';
 
 export const Container = styled.button`
   background: #ff9000;
-  height: 56px;
   border-radius: 10px;
-  border: 0;
+  border: 0px;
   padding: 0 16px;
-  color: #312e38;
   width: 100%;
-  font-weight: 500;
+  height: 56px;
+  color: #312e38;
   margin-top: 16px;
-  transition: background-color 0.2s;
+  font-weight: 500;
+  transition: all 0.2s ease-out;
 
   &:hover {
     background: ${shade(0.2, '#ff9000')};
